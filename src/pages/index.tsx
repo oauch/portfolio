d18a1d21317a452d98e428c5196583516385f434
oauch <lgm9515@naver.com>
2024-04-20 20:48:55 +0900

    ✅ Feat: 초기 화면

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,17 @@
+import AboutMeSection from "@/components/AboutMeSection";
+import Header from "@/components/Header";
+import LandingSection from "@/components/LandingSection";
+
 function Home() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <LandingSection />
+        <AboutMeSection />
+      </main>
+    </>
+  );
 }
 
 export default Home;
