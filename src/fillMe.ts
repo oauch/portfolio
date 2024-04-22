3e8b6a1138c24759220870b74bf13fca00bb134f
oauch <lgm9515@naver.com>
2024-04-22 16:20:31 +0900

    ✅ Feat: fillMe 스킬 이미지 및 alt 값

--- src/fillMe.ts
+++ src/fillMe.ts
@@ -1,5 +1,44 @@
+import CSS from "../public/icons/CSS.svg";
+import Discord from "../public/icons/Discord.svg";
+import Emotion from "../public/icons/Emotion-Light.svg";
+import Figma from "../public/icons/Figma.svg";
+import Git from "../public/icons/Git.svg";
+import GitHub from "../public/icons/GitHub.svg";
+import HTML from "../public/icons/HTML.svg";
+import JS from "../public/icons/JavaScript.svg";
+import NextJS from "../public/icons/NextJS.svg";
+import React from "../public/icons/React.svg";
+import ReactQuery from "../public/icons/ReactQuery.svg";
+import StyledComponent from "../public/icons/StyledComponents.svg";
+import TS from "../public/icons/TypeScript.svg";
+import Jotai from "../public/images/Jotai.png";
+import Zustand from "../public/images/Zustand.png";
+
 export const User = {
   name: "임윤혁",
   birth: "1999.02.02",
   email: "dkssud4930@gmail.com",
+  skills: {
+    frontEnd: [
+      { src: HTML, alt: "HTML" },
+      { src: CSS, alt: "CSS" },
+      { src: JS, alt: "JS" },
+      { src: TS, alt: "TS" },
+      { src: React, alt: "React" },
+      { src: NextJS, alt: "NextJS" },
+      { src: StyledComponent, alt: "StyledComponent" },
+      { src: Emotion, alt: "Emotion" },
+      { src: ReactQuery, alt: "ReactQuery" },
+      { src: Jotai, alt: "Jotai" },
+      { src: Zustand, alt: "Zustand" },
+    ],
+    version: [
+      { src: Git, alt: "Git" },
+      { src: GitHub, alt: "GitHub" },
+    ],
+    communication: [
+      { src: Discord, alt: "Discord" },
+      { src: Figma, alt: "Figma" },
+    ],
+  },
 };
