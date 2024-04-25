b0616b3bf0b22d883b9e2213d39f8bacd0aa2dfd
oauch <lgm9515@naver.com>
2024-04-25 18:10:12 +0900

    ✅ Feat: 프로젝트 포트폴리오

--- src/fillMe.ts
+++ src/fillMe.ts
@@ -11,20 +11,25 @@ import React from "@/icons/React.svg";
 import ReactQuery from "@/icons/ReactQuery.svg";
 import StyledComponent from "@/icons/StyledComponents.svg";
 import TS from "@/icons/TypeScript.svg";
+import Project2 from "@/images/About_P.avif";
 import Jaws7 from "@/images/Account_Jaws.avif";
 import Jaws6 from "@/images/Card_Jaws.avif";
 import Jaws5 from "@/images/DashBoard_Jaws.avif";
+import Project4 from "@/images/Hubs_P.avif";
 import Jotai from "@/images/Jotai.png";
 import Jaws1 from "@/images/Landing_Jaws.avif";
+import Project1 from "@/images/Landing_P.avif";
 import Rolling1 from "@/images/Landing_R.avif";
 import Rolling2 from "@/images/Main_R.avif";
 import Rolling5 from "@/images/Modal_R.avif";
 import Jaws4 from "@/images/MyDashboard_Jaws.avif";
 import Rolling3 from "@/images/NewPaper_R.avif";
+import Project5 from "@/images/Proeject_P.avif";
 import Rolling4 from "@/images/Rolling_R.avif";
 import Jaws8 from "@/images/Setting_Jaws.avif";
 import Jaws2 from "@/images/Signin_Jaws.avif";
 import Jaws3 from "@/images/Singup_Jaws.avif";
+import Project3 from "@/images/Skills_P.avif";
 import Rolling6 from "@/images/Write_R.avif";
 import Zustand from "@/images/Zustand.png";
 
@@ -58,7 +63,23 @@ export const USER = {
     ],
   },
   PROJECT: {
-    ONE: {
+    ROLLING: {
+      NAME: "롤링폴링",
+      DATE: "2023.11 ~ 2023.11",
+      HEADCOUNT: 4,
+      GITHUB: "https://github.com/10-rolling/rollingPolling",
+      URL: "https://rollingpolling.netlify.app/",
+      SKILLS: ["React", "Styled-Components", "Zustand"],
+      IMAGES: [
+        { src: Rolling1, alt: "초기 페이지" },
+        { src: Rolling2, alt: "메인 페이지" },
+        { src: Rolling3, alt: "새로운 페이퍼 생성 페이지" },
+        { src: Rolling4, alt: "롤링 페이퍼 페이지" },
+        { src: Rolling5, alt: "롤링 페이퍼 모달창 (자세히 보기)" },
+        { src: Rolling6, alt: "롤링 페이퍼 작성 페이지" },
+      ],
+    },
+    JAWSTIFY: {
       NAME: "Jawstify",
       DATE: "2023.12 ~ 2024.01",
       HEADCOUNT: 5,
@@ -82,20 +103,19 @@ export const USER = {
         { src: Jaws8, alt: "계정 관리 페이지" },
       ],
     },
-    SECOND: {
-      NAME: "롤링폴링",
-      DATE: "2023.11 ~ 2023.11",
-      HEADCOUNT: 4,
-      GITHUB: "https://github.com/10-rolling/rollingPolling",
-      URL: "https://rollingpolling.netlify.app/",
-      SKILLS: ["React", "Styled-Components", "Zustand"],
+    PORTFOLIO: {
+      NAME: "포트폴리오",
+      DATE: "2024.04 ~ 2024.04",
+      HEADCOUNT: 1,
+      GITHUB: "https://github.com/oauch/portfolio",
+      URL: "https://yoonhyuk.vercel.app/",
+      SKILLS: ["React", "Next.js", "Emotion"],
       IMAGES: [
-        { src: Rolling1, alt: "초기 페이지" },
-        { src: Rolling2, alt: "메인 화명" },
-        { src: Rolling3, alt: "새로운 페이퍼 생성 페이지" },
-        { src: Rolling4, alt: "롤링 페이퍼 페이지" },
-        { src: Rolling5, alt: "롤링 페이퍼 모달창 (자세히 보기)" },
-        { src: Rolling6, alt: "롤링 페이퍼 작성 페이지" },
+        { src: Project1, alt: "메인 페이지" },
+        { src: Project2, alt: "About Me" },
+        { src: Project3, alt: "Skills" },
+        { src: Project4, alt: "Hubs" },
+        { src: Project5, alt: "Project" },
       ],
     },
   },
